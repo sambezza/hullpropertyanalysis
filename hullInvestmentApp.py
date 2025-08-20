178d6c19f29c168afaa5140d78a4d2ab6ec347e0
sambezza <s.berry4983@gmail.com>
2025-08-20 16:45:54 +0100

    Update hullInvestmentApp.py
    Url update

--- hullInvestmentApp.py
+++ hullInvestmentApp.py
@@ -83,7 +83,7 @@ st.set_page_config(
 st.title("Berry & Bateman Property Enterprises - Dashboard")
 
 # Path to your existing CSV in the project
-csv_path = "ppd_data.csv"  # update this to your file path
+csv_path = "https://github.com/sambezza/hullpropertyanalysis/blob/main/pdd_data.csv"  # update this to your file path
 
 try:
     df = pd.read_csv(csv_path)
